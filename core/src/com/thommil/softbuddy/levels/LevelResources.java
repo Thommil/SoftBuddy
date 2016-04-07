522a80a0d212caf317781b18d86a6730f1bd789b
nmtv3386 <thomthom.millet@gmail.com>
2016-04-07 18:24:02 +0200

    Level load/unload

--- core/src/com/thommil/softbuddy/levels/LevelResources.java
+++ core/src/com/thommil/softbuddy/levels/LevelResources.java
@@ -2,6 +2,7 @@ package com.thommil.softbuddy.levels;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
 import com.thommil.libgdx.runtime.tools.RubeLoader;
 
 public class LevelResources extends RubeLoader {
@@ -11,10 +12,16 @@ public class LevelResources extends RubeLoader {
     }
 
     public void load(final AssetManager assetManager){
-
+        for(final ImageDef imageDef : this.getImagesDefinition()){
+            assetManager.load(imageDef.path, Texture.class);
+        }
     }
 
     public void unload(final AssetManager assetManager){
-
+        for(final ImageDef imageDef : this.getImagesDefinition()){
+            if(assetManager.isLoaded(imageDef.path)) {
+                assetManager.unload(imageDef.path);
+            }
+        }
     }
 }
